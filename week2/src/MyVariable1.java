ac195cad1d46476d2828b1f86d127afa21ab3539
hatsune_kinoshita <138754141+Hatsune08@users.noreply.github.com>
2025-04-25 22:48:36 -0700

    Add files via upload

---
+++ week2/src/MyVariable1.java
@@ -0,0 +1,9 @@
+public class MyVariable1 {
+
+    public static void main(String[] args){
+        String name = "hatsune";
+        int hours = 40;
+        System.out.println(name);
+        System.out.println(hours);
+    }
+}
